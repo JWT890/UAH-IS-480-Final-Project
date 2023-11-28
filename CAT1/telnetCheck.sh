9f09cafe1904f3431536bbd5e9e0cd0680ed2003
Jon Taylor <95875505+JWT890@users.noreply.github.com>
2023-11-28 13:21:41 -0600

    Update telnetCheck.sh

--- CAT1/telnetCheck.sh
+++ CAT1/telnetCheck.sh
@@ -10,17 +10,25 @@
 
 # STIG UBTU-20-010405 CAT 1
 
+# checks to see if telnet is installed
 if dpkg -l | grep telnet; then
+	# prints Telnet is installed
 	echo "Telnet is installed"
+ 	# asks the user if they want to uninstall telnet
 	read -p "Do you want to uninstall telnet? (y/n): " uninstall_prompt
 
+	# if user says yes it will remove telnet
 	if [[ $uninstall_prompt == "y" ]]; then
+ 		# command for removing telnet
 		sudo apt-get remove telnet
+  		# prints telnet has been uninstalled
 		echo "Telnet has been uninstalled"
 	else
-		echo "Telnet will be uninstalled"
+ 		# prints Telnet will not be uninstalled
+		echo "Telnet will be not uninstalled"
 	fi
 else
+	# prints Telnet is not installed
 	echo "Telnet is not installed"
 fi
 
