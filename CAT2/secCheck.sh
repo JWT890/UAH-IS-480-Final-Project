9a34b69f29a3be696573a172d22cf4eaf9c883cf
Jon Taylor <95875505+JWT890@users.noreply.github.com>
2023-11-28 13:24:05 -0600

    Update secCheck.sh

--- CAT2/secCheck.sh
+++ CAT2/secCheck.sh
@@ -22,11 +22,17 @@ else
 	echo "System does not have screen lock enabled and is not compliant"
 fi
 
+# statement that checks to determine if command is false
 if [[ "$checkScreenLock" != *true* ]]; then
-	read -p "Would you like to enable screen lock? y/n: " confirm 
+	# asks the user if the want to enable screen look
+	read -p "Would you like to enable screen lock? y/n: " confirm
+ 	# statement takes either yes or no for answer
 	if [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]]; then
+ 		# command sets condition to true
 		sudo gsettings set org.gnome.desktop.screensaver lock-enabled true
+  		# prints screen lock enabled
 		echo "screen lock enabled" 
+  	# prints screen lock not enabled
 	else exit 1 && echo "screen lock not enabled"
 	fi
 fi
